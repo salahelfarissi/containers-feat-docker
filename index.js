3eea7eb070e062d7949b4a8a45ec161f63702453
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-08-14 13:14:48 +0200

    Replace code with hapi-pino example

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const http = require("http");
+// more-or-less the example code from the hapi-pino repo
+const hapi = require("@hapi/hapi");
 
-http
-  .createServer(function (request, response) {
-    console.log("request received");
-    response.end("omg hi", "utf-8");
-  })
-  .listen(3000);
-console.log("server started");
+async function start() {
+  const server = hapi.server({
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler() {
+      return { success: true };
+    },
+  });
+
+  await server.register({
+    plugin: require("hapi-pino"),
+    options: {
+      prettyPrint: true,
+    },
+  });
+
+  await server.start();
+
+  return server;
+}
+
+start().catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
